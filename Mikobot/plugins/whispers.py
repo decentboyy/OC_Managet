459ab7de46db4f1fb35ef6f67f7c985e35c6fa66
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2024-02-05 15:55:31 +0530

    Update whispers.py

--- Mikobot/plugins/whispers.py
+++ Mikobot/plugins/whispers.py
@@ -174,8 +174,8 @@ __help__ = """
 
 ➠ *Commands:*
 
-» @MIKO_V2BOT your message @username
-» @MIKO_V2BOT @username your message
+» @Bot_username your message @username
+» @Bot_username @username your message
 """
 
 __mod_name__ = "WHISPER-MSG"

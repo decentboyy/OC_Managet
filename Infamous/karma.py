2ab64fc944765d28913bf67f40a973d92a41ad4c
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2024-02-05 16:51:40 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,31 +10,31 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://graph.org/file/07dcf96beca557f37e78c.jpg",
+    "https://graph.org/file/02f3b8a4f40f2ce2db693.jpg",
+    "https://graph.org/file/b2e13a93a2b8cad24978e.jpg",
+    "https://graph.org/file/4f7e3f5cce4c3a178992c.jpg",
+    "https://graph.org/file/e780bc4cae78ca6ba3c82.jpg",
+    "https://graph.org/file/b8accaf1fc32215a95c8d.jpg",
+    "https://graph.org/file/0805fd87e2ef2db8fa652.jpg",
 ]
 
-HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
+HEY_IMG = "https://te.legra.ph/file/098422ced35d8802b27b6.jpg"
 
 ALIVE_ANIMATION = [
-    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
-    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
-    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
-    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+    "https://graph.org/file/8f93752bc09d96a7dc599.mp4",
+    "https://graph.org/file/5fdc28db1660385c38873.mp4",
+    "https://graph.org/file/831ab5e16ee2ff217806e.mp4",
+    "https://graph.org/file/e0bd50ce6a2ee2181b4cc.mp4",
+    "https://graph.org/file/91220284ae0d2a6779b21.mp4",
+    "https://graph.org/file/ed5dda3c26640f7b98a8c.mp4",
+    "https://graph.org/file/773d73d8a4be70e8a1e5d.mp4",
+    "https://graph.org/file/2f34604a921231d494c47.mp4",
 ]
 
 FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
 
-PM_START_TEXT = "✨ *ɪ ᴀᴍ ᴍɪᴋᴏ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+PM_START_TEXT = "✨ *ɪ ᴀᴍ TG Manager, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
 
 START_BTN = [
     [
@@ -71,8 +71,8 @@ GROUP_START_BTN = [
 
 ALIVE_BTN = [
     [
-        ib(text="UPDATES", url="https://t.me/Hydra_Updates"),
-        ib(text="SUPPORT", url="https://t.me/hydraXsupport"),
+        ib(text="UPDATES", url="https://t.me/CodeNexus_community"),
+        ib(text="SUPPORT", url="https://t.me/Iink_ka_adda"),
     ],
     [
         ib(
